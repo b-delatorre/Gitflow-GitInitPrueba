325f9c0569b942f0df47727b959267bd1f62848d
Beatriz DelaTorre <9106769@alumnos.ufv.es>
2022-09-24 15:08:44 +0200

    version0.1

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String nombre = LeerNombre(entrada);
-        int edad = LeerEdad(entrada);
-        System.out.println("Hola " + nombre + " tienes  " + edad + " años.");
+        String fechaNacimiento = LeerFechaNacimiento(entrada);
+        System.out.println("Hola " + nombre + " naciste el  " + fechaNacimiento + " .");
     }
     private static String LeerNombre(Scanner entrada){
         System.out.println("Dime tu nombre: ");
         String nombre = entrada.nextLine();
         return nombre;
     }
-    private static int LeerEdad(Scanner entrada){
-        int edad = entrada.nextInt();
-        return edad;
+    private static String LeerFechaNacimiento(Scanner entrada){
+        System.out.println("Introduce tu fecha de nacimiento en el formato (DD/MM/AAAA): ");
+        String fechaNacimiento = entrada.nextLine();
+        return fechaNacimiento;
     }
 }
